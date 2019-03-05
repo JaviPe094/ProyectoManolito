32b260ad47a13ae27e82c9b640a21858495c5c59
JaviP <JaviP>
2019-03-05 13:14:44 +0100

    actualizado manolito_db.sql.Añadida tabla estados y sentencias para hacer que empleado.estado sea un FK que recoja su valor de la tabla estados

--- DOC/manolito_db.sql
+++ DOC/manolito_db.sql
@@ -2,6 +2,11 @@ CREATE TABLE Permisos(
     nombre VARCHAR2(15) NOT NULL PRIMARY KEY
 );
 
+CREATE TABLE Estados(
+    estado CHAR(1)NOT NULL PRIMARY KEY,
+    descipcion VARCHAR(50) NULL
+);
+
 CREATE TABLE Empleados(das VARCHAR2(15) NOT NULL PRIMARY KEY,
                     password VARCHAR2(20) NOT NULL,
                     nombre VARCHAR2(20) NOT NULL,
@@ -9,17 +14,27 @@ CREATE TABLE Empleados(das VARCHAR2(15) NOT NULL PRIMARY KEY,
                     email VARCHAR2(40) NULL,
                     estado CHAR NOT NULL,
                     permiso VARCHAR(15) NOT NULL,
+                    CONSTRAINT FK_EstadoEmpleado FOREIGN KEY (estado)
+                    REFERENCES Estados(estado),
                     CONSTRAINT FK_PermisoEmpleado FOREIGN KEY (permiso)
                     REFERENCES Permisos(nombre)
                     );
+
+ALTER TABLE Empleados ADD FOREIGN KEY (estado) REFERENCES Estados(estado);
                     
                     
 CREATE TABLE Tareas(
                     nombre VARCHAR2(15) NOT NULL PRIMARY KEY,
                     descripcion VARCHAR2(50) NOT NULL,
                     estado CHAR NOT NULL);/*No me ha aceptado BOOLEAN como tipo de dato*/
+
+
 					
                     
 insert into permisos values('b');
                     
 insert into empleados values('c00001', 'pepe', 'pepe', 'pepe', 'pepe@p.es', 'i', 'b');
+
+insert into estados values('a','Usuario activo.Puede acceder a la aplicación');
+insert into estados values('i','Usuario inactivo.No puede acceder a la aplicación');
+insert into estados values('n','Usuario nuevo.Necesita una nueva contraseña');
