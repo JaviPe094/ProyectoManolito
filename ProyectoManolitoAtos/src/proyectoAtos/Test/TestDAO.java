69b364ff5bcc2e110b399d710cde9b011db6a452
JaviP <JaviP>
2019-03-05 12:52:19 +0100

    nuevo TestDAO completado.Comprobado DAO de Permisos.Al tener un solo campo,habría que cambiar el método update si queremos usarlo

--- ProyectoManolitoAtos/src/proyectoAtos/Test/TestDAO.java
+++ ProyectoManolitoAtos/src/proyectoAtos/Test/TestDAO.java
@@ -4,11 +4,45 @@ import proyectoAtos.Entidades.*;
 import proyectoAtos.Modelo.*;
 
 public class TestDAO {
-	EmpleadoDAO edao=new EmpleadoDAOImpl();
-	PermisosDAO pdao=new PermisosDAOImpl();
 	
-	Empleados emp=new Empleados();
-	Permisos per=new Permisos();
+	public static void main(String[] args) {
+		
+		EmpleadoDAO edao=new EmpleadoDAOImpl();
+		PermisosDAO pdao=new PermisosDAOImpl();
+		
+		Empleados emp=edao.read("00a00b00c");
+		Permisos per=pdao.read("Administrador");
+		
+		System.out.println(per);
+		
+//		per.setNombre("Empleado");
+
+//		pdao.create(per);
+//		
+//		System.out.println(pdao.read("Empleado"));
+		
+//		per.setNombre("Usuario");
+//		
+//		pdao.update(per);
+		
+		System.out.println(pdao.read("Usuario"));
+		
+		pdao.delete("Usuario");
+		
+//		emp=new Empleados("001002003", "prueba2", "prueba2", "prueba23", "prueba2@pr.pr", 'i', per);
+//		
+//		edao.create(emp);
+//		
+//		System.out.println(edao.read("001002003"));
+//		
+//		emp.setNombre("Javier");
+//		
+//		edao.update(emp);
+//		
+//		System.out.println(edao.read("001002003"));
+//		
+//		//edao.delete("001002003");
+	}
 	
-	emp=edao.read("00a00b00c");
+	
 }
