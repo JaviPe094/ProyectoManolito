a5d35d0aab5cb6299de39086541ad6b3c894d517
Damasko <damasko015@yahoo.es>
2019-03-06 15:29:19 +0100

    Fixed formulario_login en web.xml. Creado EmpleadoUtil, creado método generatePass

--- ProyectoManolitoAtos/src/proyectoAtos/recursos/EmpleadoUtil.java
+++ ProyectoManolitoAtos/src/proyectoAtos/recursos/EmpleadoUtil.java
@@ -1,5 +1,30 @@
 package proyectoAtos.recursos;
 
+import javax.crypto.KeyGenerator;
+import java.util.Random;
+
 public class EmpleadoUtil {
+	
+	private static Random random;
+	
+	static {
+		
+		random = new Random();
+		
+	}
+	
+	public static String generatePass() {
+		
+		String res = "";
+		
+		for (int i = 0; i < 10; ++i) {
+			
+			res += Character.toChars(random.nextInt((255 - 1) + 1) + 255); 
+			
+		}
+		
+		return res;
+		
+	}
 
 }
