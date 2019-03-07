59ec5e575f60073ae88f57d3964eb5ef681ca3b4
jcdiazmo <jcdiazmo93@gmail.com>
2019-03-07 13:19:45 +0100

    ClaseControladorActualizada y WebContent

---
+++ ClaseControladorActualizada/Controlador.java
@@ -0,0 +1,344 @@
+	package proyectoAtos.Controlador;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import proyectoAtos.Entidades.Empleados;
+import proyectoAtos.Entidades.Estado;
+import proyectoAtos.Entidades.Permisos;
+import proyectoAtos.Entidades.Tareas;
+import proyectoAtos.Modelo.EmpleadoDAO;
+import proyectoAtos.Modelo.EmpleadoDAOImpl;
+import proyectoAtos.Modelo.EstadoDAO;
+import proyectoAtos.Modelo.EstadoDAOImpl;
+import proyectoAtos.Modelo.TareasDAO;
+import proyectoAtos.Modelo.TareasDAOImpl;
+import proyectoAtos.Recursos.EmpleadoUtil;
+import proyectoAtos.Recursos.Recursos;
+
+/**
+ * Servlet implementation class Controlador
+ * 
+ */
+
+@WebServlet("/Controlador")
+public class Controlador extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	EmpleadoDAO dao;
+	TareasDAO tareasDAO;
+	Empleados userLogin;
+
+	EstadoDAO estadoDAO;
+	Estado estado;
+
+	/* Contructor de Controlador */
+
+	public Controlador() {
+
+		super();
+
+		dao = new EmpleadoDAOImpl();
+
+		userLogin = new Empleados();
+
+		estadoDAO = new EstadoDAOImpl();
+
+		tareasDAO = new TareasDAOImpl();
+
+	}
+
+	/*
+	 * Tanto doPost como doGet son necesarios. En este caso, por evitar que los
+	 * datos de formulario salgan en la barra de direcciones se ha optado por
+	 * doPost. doGet queda vacío.
+	 * 
+	 */
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	/*
+	 * La funcionalidad se encuentra en el método doPost. Se ha optado por el
+	 * enfoque de un servlet encargado de las funcionalidades frente a varios
+	 * servlets, con el objetivo de unificar las operaciones
+	 * 
+	 */
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		// leer comando del formulario
+		String comando = request.getParameter("instruccion");
+
+		request.getSession().setMaxInactiveInterval(10);
+		// Salida rápida en caso de que se produzca un error en el comando
+
+		if (comando == null) {
+
+			System.out.println("El comando esta vacio");
+
+			response.sendRedirect("formulario_login.html");
+
+			return;
+
+		}
+
+		switch (comando) {
+
+		case "validar":
+
+			validarLogin(request, response);
+			break;
+
+		case "usuarioAceptado":
+
+			if (cambiarPass(request, response)) {
+
+				if ((userLogin.getPermiso().getNombre()).equals("admin"))
+					enviarInfoPanelAdmin(request, response);
+
+				else
+					enviarInfoPanelUser(request, response);
+
+			}
+
+			else {
+				System.out.println("FALLO AL CAMBIAR PASSWORD");
+			}
+
+			break;
+
+		case "listarEmpleados":
+			try {
+
+				enviarInfoPanel(request, response);
+
+				List<Empleados> listaEmpleados = dao.seleccionaTodos();
+
+				request.setAttribute("LISTAREMPLEADOS", listaEmpleados);
+
+				RequestDispatcher miDispa = request.getRequestDispatcher("/panel_admin_usuarios.jsp");
+
+				miDispa.forward(request, response);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			break;
+
+		case "listarTareas":
+			try {
+				enviarInfoPanel(request, response);
+
+				List<Tareas> listaTareas = tareasDAO.seleccionaTodos();
+
+				System.out.println(listaTareas);
+
+				request.setAttribute("LISTARTAREAS", listaTareas);
+
+				RequestDispatcher miDispaTareas = request.getRequestDispatcher("/panel_admin_tareas.jsp");
+
+				miDispaTareas.forward(request, response);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			break;
+			
+		case "insertar_usuario":
+			
+			String das = request.getParameter("das");
+			String nombre = request.getParameter("nombre");
+			String apellido = request.getParameter("apellidos");
+			String email = request.getParameter("email");
+			String per = request.getParameter("permisos");
+			
+			Empleados newEmpleado = new Empleados(das, EmpleadoUtil.generatePass(8), nombre, apellido, email, new Estado('n'), new Permisos(per));
+			
+			dao.create(newEmpleado);
+			
+			break;
+			
+		case "insertar_tarea":
+			
+			
+			String nomb = request.getParameter("nombre");
+			String descripcion = request.getParameter("descripcion");
+			char estado = request.getParameter("estado").charAt(1);
+			
+			
+			Tareas newTareas = new Tareas(nomb, descripcion,new Estado(estado));
+			
+			tareasDAO.create(newTareas);
+			
+		
+			
+		case "logout":
+			
+			onLogOut(request, response);
+			
+			break;
+
+		default:
+			break;
+
+		}
+
+	}
+
+	private void enviarInfoPanel(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String das = userLogin.getDas();
+		String nombre = userLogin.getNombre();
+		String apellido = userLogin.getApellido();
+		String email = userLogin.getEmail();
+
+		request.setAttribute("das", das);
+		request.setAttribute("nombre", nombre);
+		request.setAttribute("apellido", apellido);
+		request.setAttribute("email", email);
+	}
+
+	private void enviarInfoPanelUser(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String das = userLogin.getDas();
+		String nombre = userLogin.getNombre();
+		String apellido = userLogin.getApellido();
+		String email = userLogin.getEmail();
+
+		request.setAttribute("das", das);
+		request.setAttribute("nombre", nombre);
+		request.setAttribute("apellido", apellido);
+		request.setAttribute("email", email);
+		
+		List<Tareas> listaTareas = tareasDAO.seleccionaTodos();
+
+		//System.out.println(listaTareas);
+
+		request.setAttribute("LISTARTAREAS", listaTareas);
+
+		RequestDispatcher requesDis = request.getRequestDispatcher("panel_usuario.jsp");
+		requesDis.forward(request, response);
+
+	}
+
+	private boolean cambiarPass(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String pass = request.getParameter("contrasena");
+
+		userLogin.setEstado(estadoDAO.read('a'));
+
+		userLogin.setPassword(pass);
+		dao.update(userLogin);
+
+		return userLogin.getPassword().equals(pass);
+
+	}
+
+	public boolean validarLogin(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Recuperamos los parametros
+		String user = request.getParameter("usuario");
+		String pass = request.getParameter("contra");
+
+		try {
+
+			userLogin = dao.read(user);
+
+			if (userLogin != null && userLogin.getPassword().equals(pass)) {
+
+				if ((userLogin.getPermiso().getNombre()).equals("admin")) {
+					esAdmin(request, response);
+				}
+
+				else if ((userLogin.getPermiso().getNombre()).equals("basico")) {
+
+					if ((userLogin.getEstado().getEstado()) == Recursos.NUEVO) {
+
+						response.sendRedirect("nueva_contrasena.html");
+
+					}
+
+					else {
+
+						enviarInfoPanelUser(request, response);
+					}
+				}
+
+			}
+
+			else {
+
+				System.out.println("FALLO EN EL LOGIN");
+				response.sendRedirect("Login_Incorrecto.jsp");
+
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+		return user.equals(pass);
+
+	}
+
+	private void esAdmin(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		if ((userLogin.getEstado().getEstado()) == Recursos.NUEVO) {
+
+			response.sendRedirect("nueva_contrasena.html");
+
+		} else {
+			enviarInfoPanelAdmin(request, response);
+		}
+
+	}
+
+	private void enviarInfoPanelAdmin(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String das = userLogin.getDas();
+		String nombre = userLogin.getNombre();
+		String apellido = userLogin.getApellido();
+		String email = userLogin.getEmail();
+
+		request.setAttribute("das", das);
+		request.setAttribute("nombre", nombre);
+		request.setAttribute("apellido", apellido);
+		request.setAttribute("email", email);
+
+		RequestDispatcher requesDis = request.getRequestDispatcher("panel_administrador.jsp");
+		requesDis.forward(request, response);
+
+	}
+	
+	private void onLogOut(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		request.getSession().invalidate();
+		response.sendRedirect("formulario_login.jsp");
+		
+	}
+
+}
---
+++ ClaseControladorActualizada/WebContent/panel_admin_usuarios.jsp
@@ -0,0 +1,264 @@
+<%@page import="proyectoAtos.Entidades.Empleados"%>
+<%@page import="proyectoAtos.Entidades.Tareas"%>
+<%@page import="java.util.List"%>
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Panel Administracion Usuarios</title>
+	<style type="text/css">
+		
+			html{
+		
+		background-color: #648CE9;
+	}
+		body{
+		position: relative;
+		border: solid #223DBB 3px;
+		margin:20px auto auto;
+		background-color: white;
+		width: 950px;	
+		height: 780px;
+		padding: 10px;
+		
+	}
+	
+		
+		#marcoInfo{
+			
+			position: relative;
+			background-color: #167CDC;
+			width: 400px;
+			height: 300px;
+			padding: 20px;	
+		}
+		
+		#imagenUser{
+			border: solid #223DBB 3px;
+			position: absolute;
+			margin-top: -290px;
+			margin-left: 250px;
+			width: 150px;
+			height: 170px;
+			background-color: #FFFFFF;
+		}
+		
+		#img{
+			margin-top: 5px;
+			margin-left: -4px;
+		}
+		
+		div h2{
+			
+			margin-top: -60px;
+			border-bottom: dotted #0055AF;
+		}
+			
+		form{
+			
+			
+			margin-left: 13px;
+		}
+		
+		#botonEnviar{
+			margin-top: 20px;
+			margin-left: 60px;
+			padding: 7px;
+			font-size: 14px;
+			background-color: #69B2FF;
+			
+		}
+		
+		p{
+			margin-top: 8px;
+			font-family: Constantia, "Lucida Bright", "DejaVu Serif", Georgia, "serif";
+			font-weight: 200;
+			font-size: 14px;
+			color: #FFFFFF;
+		}
+		
+		input{
+			
+			font-size: 14px;
+			padding: 2px;
+		}
+		
+		#marcoOperaciones{
+			position: absolute;
+			float: right;
+			background-color: #167CDC;
+			margin-top: -340px;
+			width: 500px;
+			height: 340px;
+			margin-left: 450px;
+		}
+		#cabecera{
+			
+			position: absolute;
+			
+		}
+		
+		#parteAbajo{
+			
+			position: absolute;
+			width: 950px;
+			height: 40px;
+			margin-top: 5px;
+		}
+		#marcoTareas{
+			position: relative;
+			background-color: #167CDC;
+			width: 100%;
+			height: 420px;
+			margin-top: 10px;
+		}
+		
+		#fondoTabla{
+			position: absolute;
+			background-color: #FFFFFF;
+			margin-top:20px;
+			margin-left:50px;
+			height: 350px;
+			width: 90%;
+		}
+		
+		#titulo{
+			position: absolute;
+			height: 20px;
+			background-color: #0646B5;
+			margin-top: 20px;
+			padding: 10px;
+			margin-left: 30px;
+			width: 400px;
+			border: dashed #FFFFFF;
+		}
+		#operaciones{
+			
+			position: relative;
+			color: #FFFFFF;
+			margin-left: 150px;
+			font-size: 25px;
+			margin-top: -5px;
+			
+		}
+		
+		#botonAccionUser{
+			
+			position: relative;
+			margin-top: 100px;
+			width: 300px;
+			margin-left: 60px;
+			
+		}
+		
+		#botonAccionUser input{
+			
+			width: 100%;
+		}
+		
+		#botonesAccion{
+			
+			position: absolute;
+			margin-top: -30px;
+			margin-left: 50px;
+		}
+		
+		#tabla{
+			position: absolute;
+			width: 100%;
+			margin-top: 1px;
+			
+		}
+		
+		
+	
+	</style>
+</head>
+
+<%
+	//obtener los registros de Empleados
+	List<Empleados> listaEmpleados = (List<Empleados>) request.getAttribute("LISTAREMPLEADOS");
+%>
+
+
+<body bgcolor="#ADD5FF">
+	
+	<div id="cabecera">
+	
+	<div id="marcoInfo">
+		
+		<h3>DATOS ADMINISTRADOR</h3>
+		<p>DAS:</p><input type="text" value="${das}">
+		<p>NOMBRE:</p><input type="text" value="${nombre}">
+		<p>APELLIDO:</p><input type="text" value="${apellido}">
+		<p>E-MAIL:</p><input type="text" value="${email}">
+		
+		<div id="imagenUser">
+			<div id="img"><img width="160px" src="img/User_icon_2.svg.png"></div>
+		</div>
+		
+	
+	</div>
+	<div id="marcoOperaciones">
+		
+		<div id="titulo"><div id="operaciones">Operaciones</div>
+		
+		<div id="botonAccionUser">
+			<form action="Controlador" method="post">
+			<input type="submit" value="VISTA USUARIOS">
+			<input type="hidden" name="instruccion" value="listarEmpleados">
+			</form>
+			<br><br>
+			<form action="Controlador" method="post">
+			<input type="submit" value="VISTA TAREAS">
+			<input type="hidden" name="instruccion" value="listarTareas">
+			</form>
+		</div>
+	
+	</div>
+		
+	</div>
+	
+	<div id="parteAbajo">
+	<div id="marcoTareas">
+		<div id="fondoTabla">
+		
+			<table id="tabla" width="100%" border="2">
+  <tbody>
+   <tr>
+      <th>DAS</th>
+      <th>NOMBRE</th>
+      <th>APELLIDO</th>
+      <th>EMAIL</th>
+      <th>ESTADO</th>
+      <th>PERMISOS</th>
+      
+    </tr>
+    
+    <% for(Empleados emp : listaEmpleados){%>
+
+		 <tr>
+		 	<td><%=emp.getDas()%></td>
+		 	<td><%=emp.getNombre()%></td>
+		 	<td><%=emp.getApellido()%></td>
+		 	<td><%=emp.getEmail()%></td>
+		 	<td><%=emp.getEstado().getEstado()%></td>
+		 	<td><%=emp.getPermiso().getNombre()%></td>	 	
+		 
+    </tr>
+    <%} %>
+  </tbody>
+</table>
+
+			
+	</div>
+	</div>
+	<div id="botonesAccion"><input type="button" value="Insertar Usuario" onclick="window.location.href='insercion_usuarios.html'">&nbsp;&nbsp;&nbsp;
+	</div>
+	</div>
+			
+</body>
+</html>
